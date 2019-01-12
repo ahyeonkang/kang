9c6fd115f96befedf94a6aace88836aaa7502924
kang <smrdb@naver.com>
2019-01-12 18:25:47 +0900

    과제 commit

--- js/week1-js.js
+++ js/week1-js.js
@@ -22,6 +22,9 @@ var books = [
   {title: "러닝 리액트", price: 28000, author: "알렉스 뱅크스", publisher: "한빛미디어"},
 ];
 
+
+//mdn참고!!!!!!!
+
 // 2. books의 type은 무엇인가? 자바스크립트의 타입은 몇가지가 있는가?
 
 
@@ -68,7 +71,9 @@ console.log(index);
 
 // 11. 책의 총 비용을 출력하시오
 console.log('11 -----------------------------------------------');
-var sum = 0;
+// var sum = 0;
+let sum = 0;
+books.forEach(item => sum += item.price);
 
 console.log('sum:' + sum);
 
@@ -92,3 +97,80 @@ console.log(tempBooks);
 //   '7 인사이드 자바스크립트',
 //   '8 자바의 정석',
 //   '9 핸즈온 머신러닝' ]
+
+
+let book = {title: "인사이드 자바스크립트", price: 18000, author: "송영주", publisher: "한빛미디어"};
+
+let array = [
+  {id: 1, name: 'aaa'},
+  {id: 2, name: 'bbb'},
+  {id: 3, name: 'ccc'},
+  {id: 4, name: 'ddd'},
+  {id: 5, name: 'eee'},
+  {id: 6, name: 'fff'},
+  {id: 7, name: 'ggg'},
+  {id: 8, name: 'hhh'},
+  {id: 9, name: 'iii'},
+];
+
+// 1. book을 카피한후에  price를 바꾸면 book과 copyBook이 같이 바뀐다. 이유가 무엇인가?
+// 이런 카피를 머라고 부르는가?
+let copyBook = book;
+copyBook.price = 20000;
+console.log(book);
+console.log(copyBook);
+
+// 2. book을 카피하되 deep copy를 수행한 후에 price를 30000으로 바꾸고 출력하라. 두개의 값이 달라야 한다.
+console.log('2 -------------------------------------------------------------------');
+// 2-1 Object.assign() 사용하여 deep copy하기
+let deepCopyBookEs5;
+// 2-2 es6 spread 연산자를 사용하여 deep copy 하기
+let deepCopyBookEs6;
+
+console.log(book);
+console.log(deepCopyBookEs5);
+console.log(deepCopyBookEs6);
+
+// 3. destructing을 사용하여 book의 title, author 변수를 한번에 받는 코드를 작성하시오.
+console.log('3 -------------------------------------------------------------------');
+
+console.log(title, author);
+
+// 4. id가 2의 배수의 집합인 A를 구하시오. id가 3의 배수의 집합인 B를 구하시오.
+console.log('4 -------------------------------------------------------------------');
+let A;
+let B;
+console.log(A);
+console.log(B);
+
+// 5. 교집합 구하기 A & B
+console.log('5 -------------------------------------------------------------------');
+let intersection;
+console.log(intersection);
+
+// 6. 차집합 구하기 A - B
+console.log('6 -------------------------------------------------------------------');
+let difference;
+console.log(difference);
+
+// 7. es6의 Set 객체를 사용하여 A와 B의 합집합을 구하시오
+console.log('7 -------------------------------------------------------------------');
+// let a = new Set([1,2,3]);
+// let b = new Set([4,3,2]);
+// let union = new Set([...a, ...b]);
+
+console.log(union);
+// [ { id: 2, name: 'bbb' },
+//   { id: 4, name: 'ddd' },
+//   { id: 6, name: 'fff' },
+//   { id: 8, name: 'hhh' },
+//   { id: 3, name: 'ccc' },
+//   { id: 9, name: 'iii' } ]
+
+
+// 8. id가 4의 배수의 배열을 구하되 아래와 같이 name 키가 아니라 name + id 로 구성된 배열을를 구하시오.
+// [hint]: computed property
+console.log('8 -------------------------------------------------------------------');
+let C;
+console.log(C);
+// [ { id: 4, name4: 'ddd' }, { id: 8, name8: 'hhh' } ]
