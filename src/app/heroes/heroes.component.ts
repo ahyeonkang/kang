05ae2e99740a0f4069b05eb3ad90b2c580e3314b
kang <smrdb@naver.com>
2019-01-12 17:15:06 +0900

    binding commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+// import { Hero } from '../hero'
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+
+  // hero = "winstorm"
+  // hero: Hero = {
+  //   id: 11,
+  //   name: 'Winstorm'
+  // };
+  hero: Hero;
+
+  isSpecial = false;
+
+  constructor() {
+    this.hero = new Hero(11,'winstorm');
+  }
+
 
   ngOnInit() {
   }
